3592c3e0db9266b27a6c2f5d4f60df6eb94ea102
Eduardo Rosas <lalorosas@outlook.com>
2017-12-22 12:00:18 -0600

    Agregar archivos de proyecto.

---
+++ CSharpConsola/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroUno = int.Parse(Console.ReadLine());
+            int numeroDos = int.Parse(Console.ReadLine());
+            int resultado = Suma(numeroDos, numeroUno);
+            Console.WriteLine($"Resultado: {resultado}");
+            Console.ReadLine();
+        }
+
+        static int Suma(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+    }
+}
